8837d25286749ba08dcaae35498f9ae1f545424b
Ryan <rirvin5@gmail.com>
2024-07-24 18:43:33 -0400

    refactor database initialization

---
+++ IMS/Data/DbInitializer.cs
@@ -0,0 +1,36 @@
+﻿using IMS.Models;
+
+namespace IMS.Data
+{
+    public static class DbInitializer
+    {
+        public static void Initialize(InventoryDbContext context)
+        {
+            context.Database.EnsureCreated();
+
+            if (context.Products.Any())
+                return; //Database has been initialized
+
+            var categories = new Category[]
+            {
+                new Category {Name = "Electronics"},
+                new Category {Name = "Books"}
+            };
+
+            foreach (Category c in categories)
+                context.Categories.Add(c);
+
+            context.SaveChanges();
+
+            var products = new Product[] {
+                new Product {Name = "Laptop", Price = 999.99M, Category = categories[0]},
+                new Product {Name = "Book", Price = 19.99M, Category = categories[1]}
+            };
+
+            foreach (Product p in products)
+                context.Products.Add(p);
+
+            context.SaveChanges();
+        }
+    }
+}
